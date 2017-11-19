30145f6a968c7b5db2a2d207a50293bd9fb693cb
Manveroo <cwdrspam-github@yahoo.de>
2017-11-19 15:12:53 +0100

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {CollapseModule} from 'ngx-bootstrap';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {LoginComponent} from './login/login.component';
+import {MemoryComponent} from './memory/memory.component';
 
+
+const appRoutes: Routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'memory', component: MemoryComponent},
+  {
+    path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  }
+  // ,{path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WelcomeComponent,
+    LoginComponent,
+    MemoryComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    ),
+    BrowserModule,
+    CollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
